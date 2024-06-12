49fae865afad6429b1740bdc8d5e522ddd3b16aa
Gabriel Felipe Guarnieri <gfguarnieri@gmail.com>
2024-06-12 17:04:22 -0300

    Update route.js

--- API/routers/route.js
+++ API/routers/route.js
@@ -4,9 +4,9 @@ const userController = require('../controllers/userController');
 const listController = require('../controllers/listController');
 const authMiddleware = require('../config/authMiddleware');
 
-route.post('/user', userController.postSignUp);
-route.get('/users', userController.getUsers);
+route.post('/register', userController.postSignUp);
 route.post('/login', userController.postSignIn);
+route.get('/users', userController.getUsers);
 route.post('/list', authMiddleware, listController.postList);
 route.get('/list/:userId', authMiddleware, listController.getListsByUser);
 route.delete('/list/:listId', authMiddleware, listController.deleteList);
